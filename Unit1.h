cdd5b5c636999dc7d635bf5556742ceb96ccd309
Duvoleros <73668592+Duvoleros@users.noreply.github.com>
2022-01-07 05:35:21 +0300

    Map Generator (#1)
    Added map generator, created initial Main menu
    
    Co-authored-by: FoLZer <65248062+FoLZer@users.noreply.github.com>

--- Unit1.h
+++ Unit1.h
@@ -20,6 +20,20 @@ __published:	// IDE-managed Components
 	TImage *Screen;
 	TMainMenu *MainMenu1;
 	TMenuItem *File1;
+	TMenuItem *N1;
+	TMenuItem *Saveprogress1;
+	TMenuItem *Downloadprogress1;
+	TMenuItem *Downloadprogress2;
+	TMenuItem *Exit1;
+	TMenuItem *Settings1;
+	TMenuItem *LandSize1;
+	TMenuItem *N150x1501;
+	TMenuItem *N200x2001;
+	TMenuItem *N300x3001;
+	TMenuItem *PoVskin1;
+	TMenuItem *Rogue1;
+	TMenuItem *Rogue2;
+	TMenuItem *Knight1;
 	void __fastcall FormCreate(TObject *Sender);
 	void __fastcall FormKeyPress(TObject *Sender, System::WideChar &Key);
 private:	// User declarations
