abfd3b6d5e02cf1051090c8d5d909d0ccc2ea4f3
FoLZer <nikolaenko.gleb.044@gmail.com>
2022-01-09 09:41:34 +0300

    Add Bridge class correctly

--- Bridge.h
+++ Bridge.h
@@ -1,14 +1,14 @@
 #pragma once
+
 //---------------------------------------------------------------------------
+
 #ifndef BridgeH
 #define BridgeH
-#endif
 //---------------------------------------------------------------------------
-#include "CellObject.h"
-#include "Cell.h"
-#include "Texture.h"
-class Bridge : public Cell {
-public:
-	Bridge(Coords _loc);
-	void DoRender(TDrawingScreen* Screen,int x,int y,int width,int height);
+#endif
+
+#include "Floor.h"
+
+class Bridge : public Floor {
+
 };
